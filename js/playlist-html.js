7f2ea5e619ad7b91edf906be883fead52ce47a05
Lukas <lukasg.dasilva2014@gmail.com>
2023-05-18 10:36:36 -0300

    update 7

--- js/playlist-html.js
+++ js/playlist-html.js
@@ -1,6 +1,5 @@
 import { listOfHtml } from '../data/videos.js'
 import { loadHeader, loadMainContent } from "./main.js";
 
-
 loadHeader('../index.html', "playlist-Js.html", "arquivosPDF.html", "playlist-exercicios.html")
 loadMainContent('HTML', listOfHtml)
